048d1cd9d037eeeea61eac18af5485cdcadd38ca
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2024-03-02 15:33:15 -0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # Change the variables from True to False, based off of what access you want to give CustomChat
-Command_Line=False
-Web_Scrape=False
+Command_Line=True
+Web_Scrape=True
 # Change to false if you want to lock your edits
-Edit=False
+Edit=True
