8bbce29409652290827183ffa832731051476eb1
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2024-04-02 14:04:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
         'beautifulsoup4'
     ],
     # other metadata
-    author='Your Name',
-    author_email='elliotedebruin@gmail.com',
+    author='Mrpi314tech',
+    author_email='123scoring@gmail.com',
     description="CustomChat: An AI-powered chatbot with easy customization. Powered by a simple but effective Python AI, it opens websites, executes commands, and delivers tailored responses. Enhance your projects with CustomChat's versatility and adaptability.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
