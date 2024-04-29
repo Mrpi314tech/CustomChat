55e62d131dbd7069c1452c6f148e72629f6f4154
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2024-04-29 17:43:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CustomChat',
-    version='1.0.10',
+    version='1.0.11',
     packages=find_packages(),
     install_requires=[
         'requests',
